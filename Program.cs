9106a441442f79fc9d888f1c3c83c16e8fcda294
Jorge Angel Chavez Guzman <kokito83012@hotmail.com>
2021-05-30 16:08:47 -0500

    Intento codigo tiempo

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+
+namespace Diary_Entry
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+
+                Console.WriteLine("Introduce el texto deseado acontinuacion");
+
+                float a = float.Parse(Console.ReadLine());
+
+
+                using (StreamWriter sw = new StreamWriter("30/05/2021.txt"))
+                {
+                    CultureInfo culture = new CultureInfo("es-MX");
+                    todayAsStr = today.toString(culture);
+                }
+            }
+        }
+
+
+
+
+
